5627387773066e26ab7b9a1faed2777b6c6ae088
rafael0055 <rafaelrg_5@hotmail.com>
2024-04-30 23:43:33 -0300

    Service layer, component registration

--- src/main/java/com/rggames/teste/config/TestConfig.java
+++ src/main/java/com/rggames/teste/config/TestConfig.java
@@ -1,12 +1,10 @@
 package com.rggames.teste.config;
 
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
 import com.rggames.teste.entities.User;
 import com.rggames.teste.repositories.UserRepository;
 
@@ -14,6 +12,7 @@ import com.rggames.teste.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner{
 	
+	// declarando uma dependencia
 	@Autowired
 	private UserRepository userRepository;
 
@@ -25,9 +24,7 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		userRepository.saveAll(Arrays.asList(u1 ,u2));
-	}
-	
-	
-	
-	
+	}	
 }
+
+
---
+++ src/main/java/com/rggames/teste/services/UserService.java
@@ -0,0 +1,28 @@
+package com.rggames.teste.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rggames.teste.entities.User;
+import com.rggames.teste.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	// declarando uma dependencia
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
