7a4dfd16b4fb98b8c1247cff3eb0799a3f74f383
rafael0055 <rafaelrg_5@hotmail.com>
2024-05-03 21:04:22 -0300

    Order, Instant, ISO 8601 - Part 2

---
+++ src/main/java/com/rggames/teste/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.rggames.teste.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rggames.teste.entities.Order;
+import com.rggames.teste.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	// declarando uma dependencia
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
